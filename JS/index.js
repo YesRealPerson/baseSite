cdf94dc1a2a8e66b2d4612b03a682dc1cccb3f30
YesRealPerson <86257013+YesRealPerson@users.noreply.github.com>
2023-08-24 10:41:48 -0700

    Removed broken github stuff

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,3 @@
-new GitHubCalendar("#activity", "yesrealperson").then(() => {
-    document.getElementsByClassName("js-calendar-graph")[0].lastElementChild.remove();
-});
-
 const getSteamActivity = async () => {
     const activity = (await (await fetch("https://steamactivity.spark952.workers.dev/")).json()).response.games;
     for(let i = 0; i < activity.length; i++){
